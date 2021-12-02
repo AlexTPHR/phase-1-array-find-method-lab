91d336ebedf515d1b5ba378d187f16b1aeb537a1
Alex <alex.chart@turningpointhr.com>
2021-12-02 19:23:29 +0000

    commit tree d33f8595e928139f3464c6fa753865069b296387

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function superbowlWin(win){
+    let result = win.find(element => element.result === "W")
+    
+    return result ? result.year : undefined;}
 
+    // if (result){
+    //     return result.year;
+    // }
+    //         else {
+    //             return undefined; 
+    //     }
+    // } Above is ternary and even better than my code however now I understand
